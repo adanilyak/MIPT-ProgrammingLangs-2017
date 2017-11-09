3ca93f9621620b491229bc3cd2074d2565d3910f
Alexander Danilyak <adanilyak@gmail.com>
2017-11-09 22:01:54 +0300

    Task 2.3 (5 points)

--- 2/createTable.js
+++ 2/createTable.js
@@ -3,7 +3,6 @@ function createTable() {
   table.style.border = '1px solid black';
 
   var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
-  console.log(letters)
 
   for(var i = 0; i < 21; i++) {
     var tr = table.insertRow(i);
@@ -27,10 +26,15 @@ function createTable() {
 
       td.setAttribute("contenteditable", "true");
       td.addEventListener('keypress', onKey);
+      td.id = letters[j - 1] + i;
     }
   }
 
   document.body.appendChild(table);
+}
+
+function len(param) {
+  return param.length
 }
 
 function onKey(e) {
@@ -38,9 +42,50 @@ function onKey(e) {
   if (key === 13) { // 13 is enter
     e.preventDefault();
     var td = e.path[0];
-    var result = eval(td.innerText);
-    if(result) {
-      td.innerText = result;
+    if (td.innerText.charAt(0) === '=') {
+      td.innerText = td.innerText.substr(1);
+      td.innerText = calculate(td.innerText);
     }
   }
 }
+
+function calculate(text) {
+  var input = text;
+  var regexp = /[A-Z][0-9]+/g;
+  var result = input.match(regexp);
+
+  if(result == null) {
+    input = replaceFunctions(input);
+    return safeEval(input);
+  } else {
+    for(var i = 0; i < result.length; i++) {
+      var td = document.getElementById(result[i]);
+      var calculated = calculate(td.innerText);
+      input = input.replace(result[i], calculated);
+    }
+    input = replaceFunctions(input);
+    return safeEval(input);
+  }
+
+}
+
+function replaceFunctions(text) {
+  var input = text;
+  input = input.replace(/ABS/g, 'Math.abs');
+  input = input.replace(/SIN/g, 'Math.sin');
+  input = input.replace(/LEN/g, 'String.len');
+  return input;
+}
+
+function safeEval(text) {
+  try {
+    var result = eval(text);
+    return result;
+  } catch(error) {
+    return text;
+  }
+}
+
+String.len = function(str) {
+  return String(str).length;
+};
